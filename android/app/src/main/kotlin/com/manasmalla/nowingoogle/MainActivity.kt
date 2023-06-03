a7145caa38824c7b83f53b3af22f08902648c2ae
Manas Malla <manasmalla.dev@gmail.com>
2023-06-03 16:54:49 +0530

    initial commit
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

---
+++ android/app/src/main/kotlin/com/manasmalla/nowingoogle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.manasmalla.nowingoogle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:nowingoogle/color_schemes.g.dart';
+import 'package:nowingoogle/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: lightColorScheme,
+          fontFamily: "Google Sans"),
+      darkTheme: ThemeData(
+          useMaterial3: true,
+          colorScheme: darkColorScheme,
+          fontFamily: "Google Sans"),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        const Spacer(),
+        Transform.translate(
+          offset: const Offset(0, 54),
+          child: Container(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  "assets/io.png",
+                  width: 100,
+                ),
+                Text(
+                  "I/O '23\nExtended",
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "5th August 2023",
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Opacity(
+                      opacity: 0.5,
+                      child: Text(
+                        "Vizag",
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(12)),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 24),
+                  child: const Text("Coming Soon"),
+                )
+              ],
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Image.asset(
+            "assets/io-bridge.png",
+          ),
+        ),
+      ]),
+    );
+  }
+}
