f52540b2162d4ea47357a6216810b9affd537d5b
Sai Sampath Kumar Balivada <balivadask2000@gmail.com>
2023-06-03 21:14:56 +0530

    created dir structure for project

---
+++ lib/utils/sample.dart
@@ -0,0 +1,4 @@
+void sampleUtil(){
+  // TODO: Remove this sample util as soon as the first util method is added
+  print("This is a Sample Util");
+}
---
+++ lib/widgets/countdowntimer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CountdownTimer extends StatelessWidget {
+  const CountdownTimer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
