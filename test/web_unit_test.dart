196e921d955f12982602fb1eb66040dcd54dc7b1
Manas Malla <manasmalla.dev@gmail.com>
2023-07-25 00:21:32 +0530

    commented unit tests
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- test/web_unit_test.dart
+++ test/web_unit_test.dart
@@ -1,11 +1,11 @@
-@TestOn('browser')
+// @TestOn('browser')
 
-import 'package:flutter_test/flutter_test.dart';
-import 'package:nowingoogle/firebase_options.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:nowingoogle/firebase_options.dart';
 
-void main() {
-  test('test firebase config on web', () {
-    expect(
-        () => DefaultFirebaseOptions.currentPlatform, throwsUnsupportedError);
-  });
-}
+// void main() {
+//   test('test firebase config on web', () {
+//     expect(
+//         () => DefaultFirebaseOptions.currentPlatform, throwsUnsupportedError);
+//   });
+// }
