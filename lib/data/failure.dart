a0874630cc8c914e5110764e24bcb8eadf9e3341
Manas Malla <manasmalla.dev@gmail.com>
2023-07-25 00:17:43 +0530

    fixed the errors
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- lib/data/failure.dart
+++ lib/data/failure.dart
@@ -4,6 +4,5 @@ class Failure extends Equatable {
   final String message;
   const Failure({required this.message});
   @override
-  // TODO: implement props
   List<Object?> get props => [message];
 }
--- lib/domain/repositories/google_auth_repository.dart
+++ lib/domain/repositories/google_auth_repository.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:nowingoogle/data/failure.dart';
