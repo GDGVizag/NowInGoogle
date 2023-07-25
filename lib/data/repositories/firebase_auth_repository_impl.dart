81fa2d28ef10da2975882132027d8fc154031315
Manas Malla <manasmalla.dev@gmail.com>
2023-07-25 11:00:16 +0530

    added `ci-cd` for branch protection when pushing to main through Flut… (#5)
    …ter Test
    
    ---------
    
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- lib/data/repositories/firebase_auth_repository_impl.dart
+++ lib/data/repositories/firebase_auth_repository_impl.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 import 'package:nowingoogle/data/failure.dart';
 
